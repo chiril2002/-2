fe5edbdfc88f4c3c77e36896e9c087ae51986058
chiril200 <ionhawk397@gmail.com>
2023-09-30 19:28:51 +0300

    заданя3

--- заданя3/Program.cs
+++ заданя3/Program.cs
@@ -3,19 +3,23 @@
 // 7 -> да
 // 1 -> нет
 
-Console.Write("Введи цифру, обозначающую день недели: ");
-int Number = Convert.ToInt32(Console.ReadLine());
 
-void ValidateWeekday (int Number)
+
+
+Console.Write("Введи цифру, обозначающую день недели: ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+void CheckingTheDayOfTheWeek (int dayNumber)
  {
-  if (Number == 6 || Number == 7) 
-  {
+  if (dayNumber == 6 || dayNumber == 7)
+   {
   Console.WriteLine("(этот день выходной) -> да");
   }
-  if (Number < 1 || Number > 7) 
+  else if (dayNumber < 1 || dayNumber > 7) 
   {
     Console.WriteLine("это вообще не день недели");
   }
   else Console.WriteLine("(этот день не выходной) -> нет");
 }
 
+CheckingTheDayOfTheWeek(dayNumber);
